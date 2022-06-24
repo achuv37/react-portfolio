f21b8da7499a7610aa22c9770e434ebeaeb0362a
Achu Ajesh <achuv37@gmail.com>
2022-06-23 20:17:01 -0400

    Add Nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Nav() {
+
+  return (
+    <header>
+      <h2>
+        <a href="/">
+          Aswathy
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about">
+              About me
+            </a>
+          </li>
+          <li className="mx-2">
+            <a href="#portfolio">
+              Portfolio
+            </a>
+          </li>
+          <li className="mx-2">
+            <a href="#contact">
+              Contact me
+            </a>
+          </li>
+          <li className="mx-2">
+            <a href="#resume">
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
