932e81aa5d8b35dbc4b5d04e7cc3f4f856b739a1
Achu Ajesh <achuv37@gmail.com>
2022-06-24 21:34:34 -0400

    modified resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,7 +2,45 @@ import React from 'react';
 
 function Resume () {
   return(
-  <h2> This is my REsume</h2>
+    <section className='resume'>
+      <div>
+        <h1>
+          My Resume
+        </h1>
+      </div>
+      <div>
+        <a href={require("../../assets/document/Aswathy-Ajesh.pdf")}>
+          <h5>Download my Resume</h5>
+        </a>
+      </div>
+      <div>
+        <h5>Front-end Technologies</h5>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Bootstrap</li>
+            <li>JavaScript</li>
+            <li>JQuery</li>
+            <li>React.js</li>
+          </ul>
+          
+          <h5>Back-end Technologies</h5>
+          <ul>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>Model-View-Controller</li>
+            <li>Progressive Web Applications</li>
+          </ul>
+          
+          <h5>Database</h5>
+          <ul>
+            <li>SQL</li>
+            <li>Sequalize</li>
+            <li>NoSQL</li>
+            <li>Mongodb</li>
+          </ul>
+        </div>
+      </section>
   );
 }
 
